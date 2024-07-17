8d263df4d1025b3b38058c980bcc8d9d0094e33d
Allan Hipólito <allanhipolito66@gmail.com>
2024-07-17 19:44:22 -0300

    adavacing wiwth graphql, specifing favoritesMovies of each user

--- FakeData.js
+++ FakeData.js
@@ -1,38 +1,91 @@
 const UserList = [
   {
     id: 1,
-    name: "John Doe",
-    username: "johndoe",
-    age: 30,
-    nationality: "American"
+    name: "John",
+    username: "john",
+    age: 20,
+    nationality: "CANADA",
+    friends: [
+      {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "BRAZIL",
+      },
+      {
+        id: 5,
+        name: "Kelly",
+        username: "kelly2019",
+        age: 5,
+        nationality: "CHILE",
+      },
+    ],
   },
   {
     id: 2,
-    name: "Maria Silva",
-    username: "marias",
-    age: 25,
-    nationality: "Brazilian"
+    name: "Pedro",
+    username: "PedroTech",
+    age: 20,
+    nationality: "BRAZIL",
   },
   {
     id: 3,
-    name: "Wei Zhang",
-    username: "weizhang",
-    age: 28,
-    nationality: "Chinese"
+    name: "Sarah",
+    username: "cameron",
+    age: 25,
+    nationality: "INDIA",
+    friends: [
+      {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "BRAZIL",
+      },
+    ],
   },
   {
     id: 4,
-    name: "Olga Ivanova",
-    username: "olgaivanova",
-    age: 32,
-    nationality: "Ukrainian"
+    name: "Rafe",
+    username: "rafe123",
+    age: 60,
+    nationality: "GERMANY",
   },
   {
     id: 5,
-    name: "Liam O'Connor",
-    username: "liamo",
-    age: 27,
-    nationality: "Irish"
-  }
-]
-module.exports = { UserList }
+    name: "Kelly",
+    username: "kelly2019",
+    age: 5,
+    nationality: "CHILE",
+  },
+];
+
+const MovieList = [
+  {
+    id: 1,
+    name: "Avengers Endgame",
+    yearOfPublication: 2019,
+    isInTheaters: true,
+  },
+  {
+    id: 2,
+    name: "Interstellar",
+    yearOfPublication: 2007,
+    isInTheaters: true,
+  },
+  {
+    id: 3,
+    name: "Superbad",
+    yearOfPublication: 2009,
+    isInTheaters: true,
+  },
+  {
+    id: 4,
+    name: "PedroTech The Movie",
+    yearOfPublication: 2035,
+    isInTheaters: false,
+  },
+];
+
+module.exports = { UserList, MovieList };
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -1,15 +1,36 @@
 const { gql } = require('apollo-server')
 
 const typeDefs = gql`
-type User {
+  type User {
+    id: ID!
+    name: String!
+    username: String!
+    age: Int!
+    nationality: Nationality!
+    friends: [User]
+    favoriteMovies: [Movie]
+  }
+
+type Movie{
   id: ID!
   name: String!
-  username:String!
-  age: Int!
-  nationality:String!
+  yearOfPublication: Int!
+  isInTheaters:Boolean!
 }
   type Query{
     users:[User!]!
+    user(id: ID!): User!
+    movies:[Movie!]!
+    movie(name:String!): Movie!
+  }
+
+
+  enum Nationality{
+    BRAZIL
+    CANADA
+    INDIA 
+    GERMANY
+    ARGENTINA
   }
 
 `
