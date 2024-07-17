0c4370c0fb3ee3185a9d4df583d64bacc161c7f8
Allan Hipólito <allanhipolito66@gmail.com>
2024-07-17 18:55:29 -0300

    first commit

---
+++ FakeData.js
@@ -0,0 +1,38 @@
+const UserList = [
+  {
+    id: 1,
+    name: "John Doe",
+    username: "johndoe",
+    age: 30,
+    nationality: "American"
+  },
+  {
+    id: 2,
+    name: "Maria Silva",
+    username: "marias",
+    age: 25,
+    nationality: "Brazilian"
+  },
+  {
+    id: 3,
+    name: "Wei Zhang",
+    username: "weizhang",
+    age: 28,
+    nationality: "Chinese"
+  },
+  {
+    id: 4,
+    name: "Olga Ivanova",
+    username: "olgaivanova",
+    age: 32,
+    nationality: "Ukrainian"
+  },
+  {
+    id: 5,
+    name: "Liam O'Connor",
+    username: "liamo",
+    age: 27,
+    nationality: "Irish"
+  }
+]
+module.exports = { UserList }
